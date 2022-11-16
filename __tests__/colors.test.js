f94a92ed9a10cf2c638acba7c0aec09ce46e3915
Nathan Burgess <nathans.email.address@gmail.com>
2022-11-16 12:09:49 -0800

    first test passes

--- __tests__/colors.test.js
+++ __tests__/colors.test.js
@@ -2,11 +2,11 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('color routes', () => {
-  it.skip('/colors/purple should return ["red", "blue"]', async () => {
+  it('/colors/purple should return ["red", "blue"]', async () => {
     const resp = await request(app).get('/colors/purple');
     expect(resp.body).toEqual(['red', 'blue']);
   });
-  it.skip('/colors/orange should return ["red", "yellow"]', async () => {
+  it('/colors/orange should return ["red", "yellow"]', async () => {
     const resp = await request(app).get('/colors/orange');
     expect(resp.body).toEqual(['red', 'yellow']);
   });
